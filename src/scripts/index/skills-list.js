c47ccb1e3f3689e5fa55249fd87f29d660e4d4bb
yexpotrop <sarmientodiazluisangel@gmail.com>
2026-09-02 00:52:44 -0500

    add new features

--- src/scripts/index/skills-list.js
+++ src/scripts/index/skills-list.js
@@ -4,11 +4,25 @@ function loadSkills(){
     let containerSkills = document.getElementById("skills-container");
     
     for (let i=0; i<SKILLS.length; i++){
-        containerSkills.innerHTML += `<p>Hola brocontainerSkills</p>`
-        console.log(SKILLS[i])
+        containerSkills.innerHTML += `
+            <div class="skill-card">
+                <div class="skill-card-header">
+                    <h4>TEMA_${i<10 ? `0${i}` : i}</h4>
+                    <div>
+                        <img src="${SKILLS[i].status?'../../media/img/icon/circle.svg':'../../media/img/icon/circle.svg'}" alt="icono de estado">
+                        <span>${SKILLS[i].status?'Completado':SKILLS[i].status === false?'Pendiente':'No iniciado'}</span>
+                    </div>
+                </div>
+                <div>
+                    <h3>${SKILLS[i].title}</h3>
+                </div>
+                <hr>
+                <div>
+                    <p>${SKILLS[i].description}</p>
+                    <div></div>
+                </div>
+            </div>`
     }
-
-    
 }
 
-window.onload = loadSkills();
+window.onload = loadSkills;
