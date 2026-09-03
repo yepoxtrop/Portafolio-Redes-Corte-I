b93ca407f776c06635e34ccdac3fc8e76698f4c1
neo <lsarmiento@aciel.co>
2026-09-03 11:46:06 -0500

    update index

--- src/scripts/index/konsole.js
+++ src/scripts/index/konsole.js
@@ -1,5 +1,21 @@
-function konsole(){
+const arrayInitKonsole = [
+    "Welcome to Debiam 13 (GNU/Linux x86_64)",
+    "* Documentation:  https://www.debian.org/intro/index#community",
+    "* Management:     https://www.debian.org/intro/people",
+    "* Support:        https://www.debian.org/support"
+]
 
+function konsole() {
+    const sectionKonsole = document.querySelectorAll(".introduction-terminal");
+    let delay = 1;
+    
+    sectionKonsole.forEach(element => {
+        setTimeout(() => {
+            console.log(element);
+        }, 5000 * delay);
+        delay++;
+    });
 }
 
-setInterval(konsole, 1000);
+// Ejecuta la función cuando el DOM está listo
+window.addEventListener('DOMContentLoaded', konsole);
