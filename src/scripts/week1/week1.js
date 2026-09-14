4d9cb02f2fb127bfff185a71d23d6ed8fd5495d5
C3G4C3S4R <cegacesar316@gmail.com>
2026-09-14 14:58:27 -0500

    antes del pull

--- src/scripts/week1/week1.js
+++ src/scripts/week1/week1.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnAbrirPremisa1 = document.getElementById("btn-abrir-premisa1");
     const btnCerrarPremisa1 = document.getElementById("btn-cerrar-premisa1");
 
-    // 1. Abrir modal al hacer clic en "PREMISA 1"
+    // Abrir modal al hacer clic en "PREMISA 1"
     if(btnAbrirPremisa1 && modalPremisa1) {
         btnAbrirPremisa1.addEventListener("click", () => {
             modalPremisa1.style.display = "block";
@@ -19,11 +19,50 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    // 3. (Opcional pero recomendado) Cerrar modal al hacer clic en la zona oscura fuera de la caja blanca
+    // Cerrar modal al hacer clic en la zona oscura fuera de la caja blanca
     window.addEventListener("click", (evento) => {
         if (evento.target === modalPremisa1) {
             modalPremisa1.style.display = "none";
         }
     });
 
+});
+
+// Esperamos a que la página cargue igual que con el modal
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // --- CREACIÓN DE LOGICA DEL CARRUSEL  ---
+    const pista = document.querySelector(".carrusel-pista");
+    const slides = document.querySelectorAll(".carrusel-slide");
+    const btnPrev = document.querySelector(".btn-prev");
+    const btnNext = document.querySelector(".btn-next");
+
+    // Validamos que el carrusel exista
+    if (pista && slides.length > 0) {
+        let indiceActual = 0;
+
+        function actualizarCarrusel() {
+            // Calcula cuánto debe desplazarse la pista en porcentaje
+            const desplazamiento = -(indiceActual * 100);
+            pista.style.transform = `translateX(${desplazamiento}%)`;
+        }
+
+        btnNext.addEventListener("click", () => {
+            if (indiceActual < slides.length - 1) {
+                indiceActual++;
+            } else {
+                indiceActual = 0; // Efecto bucle: si está en el último, vuelve al inicio
+            }
+            actualizarCarrusel();
+        });
+
+        btnPrev.addEventListener("click", () => {
+            if (indiceActual > 0) {
+                indiceActual--;
+            } else {
+                indiceActual = slides.length - 1; // Efecto bucle inverso
+            }
+            actualizarCarrusel();
+        });
+    }
 });
